f8c526fffa463a858851cbe10d0c717cede517e3
prashantpaddune <padadunep@gmail.com>
2023-10-09 17:54:34 +0530

    cde: fix polyfills and cors issues
    Signed-off-by: prashantpaddune <padadunep@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,26 @@
 import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+import pluginRewriteAll from "vite-plugin-rewrite-all";
+import reactSWC from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: globalThis.process?.env.VITE_BASE ?? "/",
+  plugins: [
+    pluginRewriteAll(),
+    nodePolyfills(),
+    globalThis.process?.versions?.webcontainer ? react() : reactSWC(),
+  ],
   build: {
     chunkSizeWarningLimit: 3500,
-  }
+  },
+  server: {
+    port: 5101,
+    host: globalThis.process?.env.VITE_HOST ?? "localhost",
+    strictPort: true,
+    headers: {
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin",
+    },
+  },
 });
