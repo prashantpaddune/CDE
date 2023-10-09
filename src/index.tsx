27b3cc20c9a7e75fdf112ddd3f5cb6bd42b89069
prashantpaddune <padadunep@gmail.com>
2023-10-09 17:57:42 +0530

    cde: styles and improvments
    Signed-off-by: prashantpaddune <padadunep@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
-import { createRoot } from 'react-dom/client';
+import 'react-complex-tree/lib/style-modern.css';
+import 'dockview/dist/styles/dockview.css';
+import 'xterm/css/xterm.css';
 import './index.css';
+import { createRoot } from 'react-dom/client';
 import AppRouter from "./components";
 
 const el = document.getElementById('root');
